1036ceae272d37ec5dfa4fac0941971316117e88
monsalvepilas <gsfpdaw@gmail.com>
2019-06-12 04:38:57 +0200

    Update Tarea2_Ejercicio5.java
    Terminado el ejercicio pero tengo la duda que comento en el mismo código

--- Tarea2_Ejercicio5/src/tarea2_ejercicio5/Tarea2_Ejercicio5.java
+++ Tarea2_Ejercicio5/src/tarea2_ejercicio5/Tarea2_Ejercicio5.java
@@ -1,26 +1,76 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tarea2_ejercicio5;
 
-/**
- *
- * @author monsalve
- */
-public
-        class Tarea2_Ejercicio5 {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        
-            
-            System.out.print("Esto es un ejemplo de uso de sincronizacion entre netbeans y git");
-    }
-    
-}
+public class Tarea2_Ejercicio5 {
+
+	public static void main(String[] args) {
+
+		Scanner EntradaDatosRectanculo = new Scanner(System.in);
+
+		int alto = 0;
+		int ancho = 0;
+
+		System.out.println("Solo puedes valores entre 0 y 10 ");
+
+		do {
+
+			System.out.print("Introduce el alto del rectángulo: ");
+
+			alto = EntradaDatosRectanculo.nextInt();
+
+			if ((alto < 0) || (alto > 10)) {
+
+				System.out.println("HAS INTRODUCIDO UN VALOR NO VÁLIDO, POR FAVOR, INTRODUZCA DATOS CORRECTOS ");
+			}
+		}
+
+		while ((alto < 0) || (alto > 10));
+
+		do {
+
+			System.out.print("Introduce el ancho del rectángulo: ");
+
+			ancho = EntradaDatosRectanculo.nextInt();
+
+			if ((ancho < 0) || (ancho > 10)) {
+
+				System.out.println("HAS INTRODUCIDO UN VALOR NO VÁLIDO, POR FAVOR, INTRODUZCA DATOS CORRECTOS ");
+			}
+
+		} while ((ancho < 0) || (ancho > 10));
+
+		if ((alto == 0) || (ancho == 0)) {
+
+			System.out.print("Es imposible construir un rectángulo sin lados. El programa finalizará ");
+			System.exit(0);
+
+		}
+
+		if (alto == ancho) {
+
+			System.out.print(" El programa finalizará porque los rectángulos tienen" + " lados de distintos tamaños");
+			System.exit(0);
+
+		}
+
+		while (alto > 0) {
+
+			int ancho2 = ancho;
+			
+			/* AQUÍ NO SÉ PORQUÉ NO ME COGE EL VALOR DE LA VARIABLE "ANCHO"
+			 * DIRECTAMENTE Y SIN EMBARGO, CREANDO OTRA LO PILLA */
+
+			while (ancho2 > 0) {
+
+				if (alto % 2 == 0)
+					System.out.print(" * ");
+				else
+					System.out.print(" - ");
+				ancho2--;
+			}
+			System.out.println();
+
+			alto--;
+		}
+
+	}
